13611110e47b4f45657648f2c42a84ce194d9707
j64763 <jaqueline.a64763@gmail.com>
2024-03-20 13:06:35 -0300

    Todas as alterações

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,29 @@
 alert("Atenção! Esse site foi criado para fins educacionais, não insira informações pessoais ou espere receber produtos em casa, a loja simplesmente não existe!");
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
